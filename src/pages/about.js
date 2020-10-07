89ebce41dab79aa975dbf13caf6aa1c9ac57b8e2
loophalen <abharris@outlook.com>
2020-10-07 00:43:29 -0400

    First Commit

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,11 +2,15 @@ import React from "react"
 import { Link } from 'gatsby'
 
 import AboutBlurb from "../components/aboutBlurb"
+import Footer from "../components/footer"
+import Header from "../components/header"
 
 
 const AboutPage = () => (
     <div>
-           <AboutBlurb />
+        <Header />
+        <AboutBlurb />
+        <Footer />
     </div>
 )
 
